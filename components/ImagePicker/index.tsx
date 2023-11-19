0d270efa76e1a198a2530e7e25225e296747556b
Gracie <gracie.sharma@renegadeinsurance.com>
2023-11-19 09:41:26 +0545

    add cancel button for image upload

--- components/ImagePicker/index.tsx
+++ components/ImagePicker/index.tsx
@@ -1,59 +1,100 @@
-    import React from 'react';
-    import { View, Button, Image, Modal, StyleSheet } from 'react-native';
-    import ImagePicker, { ImagePickerResponse, ImageLibraryOptions } from 'react-native-image-picker';
+import React from "react";
+import {
+  View,
+  Button,
+  Image,
+  Modal,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import ImagePicker, {
+  ImagePickerResponse,
+  ImageLibraryOptions,
+} from "react-native-image-picker";
+import { CloseIcon } from "../core";
 
-    interface CustomImageLibraryOptions extends ImageLibraryOptions {
-    title: string;
-    }
+interface CustomImageLibraryOptions extends ImageLibraryOptions {
+  title: string;
+}
 
-    interface ImagePickerComponentProps {
-    onClose: () => void;
-    onImagePicked: (response: ImagePickerResponse) => void;
-    visible: boolean;
-    }
+interface ImagePickerComponentProps {
+  onClose: () => void;
+  onImagePicked: (response: ImagePickerResponse) => void;
+  visible: boolean;
+}
 
-    export class ImagePickerComponent extends React.Component<ImagePickerComponentProps> {
-    handleChoosePhoto = () => {
-        const options: CustomImageLibraryOptions = {
-        title: 'Select Image',
-        mediaType: 'photo',
-        };
-
-        ImagePicker.launchCamera(options, (response: ImagePickerResponse) => {
-        this.props.onImagePicked(response);
-        this.props.onClose();
-        });
+export class ImagePickerComponent extends React.Component<ImagePickerComponentProps> {
+  handleChoosePhoto = () => {
+    const options: CustomImageLibraryOptions = {
+      title: "Select Image",
+      mediaType: "photo",
     };
 
-    render() {
-        return (
-        <Modal
-            animationType="slide"
-            transparent={true}
-            visible={this.props.visible}
-            onRequestClose={this.props.onClose}
-        >
-            <View style={styles.modalContainer}>
-            <View style={styles.modalContent}>
-                {/* Your ImagePicker UI */}
-                <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
-            </View>
-            </View>
-        </Modal>
-        );
-    }
-    }
-
-    const styles = StyleSheet.create({
-    modalContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
-    },
-    modalContent: {
-        backgroundColor: 'white',
-        padding: 20,
-        borderRadius: 10,
-    },
+    ImagePicker.launchImageLibrary(options, (response: ImagePickerResponse) => {
+      this.props.onImagePicked(response);
+      this.props.onClose();
     });
+  };
+
+  handleTakePhoto = () => {
+    const options: CustomImageLibraryOptions = {
+      title: "Take Photo",
+      mediaType: "photo",
+    };
+
+    ImagePicker.launchCamera(options, (response: ImagePickerResponse) => {
+      this.props.onImagePicked(response);
+      this.props.onClose();
+    });
+  };
+
+  render() {
+    return (
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={this.props.visible}
+        onRequestClose={this.props.onClose}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <TouchableOpacity
+              style={styles.closeIcon}
+              onPress={this.props.onClose}
+            >
+              <CloseIcon />
+            </TouchableOpacity>
+
+            <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
+            <Button title="Take Photo" onPress={this.handleTakePhoto} />
+            <Button title="Cancel" onPress={this.props.onClose} />
+          </View>
+        </View>
+      </Modal>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)", // Semi-transparent background
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+  },
+  closeIcon: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+  },
+  iconImage: {
+    width: 20,
+    height: 20,
+    resizeMode: "contain",
+  },
+});
