d40c83c9a78e17ec3d26dea343abd5376de9b4e1
Gracie <gracie.sharma@renegadeinsurance.com>
2023-11-25 22:22:00 +0545

    store accessibility const

---
+++ const/Accesibility.tsx
@@ -0,0 +1,37 @@
+export const ACCESSIBILITY = [
+    {
+      id: "gender",
+      value: "gender neutral",
+      label: "Gender neutral ",
+    },
+    {
+      id: "babyFeeding",
+      value: "babyFeeding",
+      label: "Baby feeding and changing",
+    },
+    {
+      id: "disability",
+      value: "disability",
+      label: "Disabled friendly",
+    },
+    {
+      id: "free",
+      value: "free",
+      label: "Free of charge",
+    },
+    {
+      id: "senior",
+      value: "seniorFriendly",
+      label: "Senior friendly ",
+    },
+    {
+      id: "commercial",
+      value: "commercial",
+      label: "Commercial building",
+    },
+    {
+      id: "government",
+      value: "government",
+      label: "Government building",
+    },
+  ];
